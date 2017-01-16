61ef35e313aeff62a593694dc2878d8608ef2082
bryantsands <bryantsands@bryantsandstekiMacBook-Pro.local>
2017-01-15 19:56:24 -0800

    insert into schema

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Double Protein Style');
+INSERT INTO burgers (burger_name) VALUES ('Double Double Animal Style');
+INSERT INTO burgers (burger_name) VALUES ('Double Double Combo');
