7c9ffe643a8075e92507ca1ed7b289f7bf097f3d
bryantsands <bryantsands@bryantsandstekiMacBook-Pro.local>
2017-01-15 20:27:11 -0800

    routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,3 +3,26 @@ var router = express.Router();
 
 var burger = require('../models/burger')
 
+//routes on routes on routes
+router.use(express.static('public'));
+
+router.get('/', function(req, res){
+    burger.getBurgers(function(data){
+        response.render('index', {burgers: data});
+    });
+});
+
+router.post('/', function(req, res){
+    burger.addBurgers(request.body.burger_name, function(){
+        response.redirect('/')
+    });
+});
+
+router.put('/:id', function(req, res){
+    burger.devourBurger(request.params.id, function(){
+        response.redirect('/');
+    });
+});
+
+//export
+module.exports = router;
