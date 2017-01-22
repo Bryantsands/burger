b1ecbfafeb72e6cdef57ac039f840c9af09bb9ef
bryantsands <bryantsands@bryantsands.local>
2017-01-21 20:43:32 -0800

    removed query string

--- config/orm.js
+++ config/orm.js
@@ -1,34 +1,36 @@
 //import mysql connection.
-var connection = require('../config/connection.js');
+var connection = require('./connection.js');
 
-var orm = {
-    selectAll: function(table, callback){
-        var queryString = 'SELECT id, burger_name, devoured, date FROM ??';
-        connection.query(queryString, function(err,result){
+
+    function selectAll(table, callback){
+        connection.query('SELECT id, burger_name, devoured, date FROM ??', [table], function(err,data){
+            if (err){
+                throw err;
+                console.log('error at selectall')
+            }
+            callback(data);
+        });
+    }
+    function insertOne(table, columns, values, callback){
+        connection.query('INSERT INTO ?? (burger_name) VALUE (?);', [table, burger_name], function(err, data){
+            if (err){
+                 throw err;
+            }
+            callback(data);
+        });
+    }
+    function updateOne(table, id, property, value, callback){
+        connection.query('UPDATE ?? SET ?? = ? WHERE id = ?;' [table, property, value, id], function(err, data){
             if (err){
                 throw err;
             }
             callback(data);
         });
-    },
-    insertOne: function (table, burgerName, callback){
-        var queryString = 'INSERT INTO id, burger_name, VALUE ';
-        connection.query(queryString, function(err, result){
-            if (err){
-                throw err;
-            }
-            callback(data);
-        })
-    },
-    updateOne: function (table, id, burgerName, value, callback){
-        var queryString = 'UPDATE TABLE, SET ? WHERE ?';
-        connection.query(queryString, function(err, result){
-            if (err){
-                throw err;
-            }
-            callback(data);
-        })
     }
-}
-    module.exports = orm;
+
+    module.exports = {
+        selectAll: selectAll,
+        insertOne: insertOne,
+        updateOne: updateOne
+    };
 
