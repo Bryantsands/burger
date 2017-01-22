a834b7fd190d0c0de8fea198d5d57af66677bfbf
bryantsands <bryantsands@bryantsands.local>
2017-01-21 20:43:02 -0800

    fixed stuff

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var orm = require('./config/orm.js');
+var controller = require('./controllers/burgers_controller.js');
 
-
-var port = 3000;
+// var orm = require('./config/orm.js');
 
 var app = express();
+var port = 3000;
+
+debugger;
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + '/public'));
+// app.use(express.static(process.cwd() + '/public'));
 
-
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(methodOverride('_method'));
 
 //set handlebars
 var exphbs = require('express-handlebars');
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 //import routes and give server access
@@ -27,4 +28,6 @@ var routes = require('./controllers/burgers_controller.js');
 
 app.use('/', routes);
 
-app.listen(port);
+app.listen(port, function(){
+    console.log('app is listening on port ', port);
+});
